9763adcac0bce42892f499720797b0706a1ccb2f
Maxwell chen <maxwell@MaxwelldeMacBook-Air-4.local>
2026-08-27 15:14:35 +0800

    Point Apollo at local BACKEND_URL GraphQL.

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,23 +1,19 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { BACKEND_URL } from '@env';
+
+const backend = (BACKEND_URL || 'http://localhost:4000').replace(/\/$/, '');
+const graphqlUri = backend.endsWith('/graphql') ? backend : `${backend}/graphql`;
 
 const httpLink = createHttpLink({
-  // used for testing on ios simulator
-  // uri: 'http://localhost:4000/graphql',
-  // used for cloud connection
-  uri: 'https://o31edlh788.execute-api.us-east-1.amazonaws.com/',
-
-  // used for testing on real ios device, adapt to your own ip address
-  // uri: 'http://192.168.1.227:4000/graphql',
+  uri: graphqlUri,
 });
 
 const authLink = setContext(async (_, { headers }) => {
-  // Get the authentication token from storage if it exists
   const tokenStorage = await AsyncStorage.getItem('user');
   const token = tokenStorage ? JSON.parse(tokenStorage)?.token : null;
 
-  // Return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
